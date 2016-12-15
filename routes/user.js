64aaf9480a9a9bc28ad3ce286b79928fed9a065e
Gregor Majcen <majcn.m@gmail.com>
2016-12-15 22:22:22 +0100

    show name and save some additional data

--- routes/user.js
+++ routes/user.js
@@ -31,7 +31,7 @@ function routerProvider(app) {
 
   router.post('/wish', ensureLoggedIn, function(req, res, next) {
     var col = app.get('db').collection('wish_holder');
-    col.update({'email': getEmail(req.user)}, {'$set': {'wish': req.body.wish}}, {'upsert': true});
+    col.update({'email': getEmail(req.user)}, {'$set': {'wish': req.body.wish, 'picture': req.user.picture, 'name': req.user.name}}, {'upsert': true});
     res.redirect('/user');
   });
 
