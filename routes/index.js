959fa90a1fcb0dc2ce5429e01e35aa646dd00232
Gregor Majcen <majcn.m@gmail.com>
2016-12-15 17:59:38 +0100

    / redirect to /user

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ var env = {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', env: env });
+  res.redirect('/user')
 });
 
 router.get('/login',
