0f8f9591a675f8137ae477f86fa7bdb4ee535c48
Gregor Majcen <majcn.m@gmail.com>
2016-12-15 20:50:39 +0100

    fix emails

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,23 @@ var passport = require('passport');
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 var router = express.Router();
 
+function getEmail(user) {
+  if (data.email) {
+    return data.email;
+  }
+
+  if (user.emails) {
+    return data.emails[0].value
+  }
+
+  throw new Error("there is no email for this user")
+}
+
 function routerProvider(app) {
   /* GET user profile. */
   router.get('/', ensureLoggedIn, function(req, res, next) {
     var col = app.get('db').collection('test');
-    var wish = col.findOne({'email': req.user.email})
+    var wish = col.findOne({'email': getEmail(req.user)})
     var strWish = ''
     if (wish) {
       strWish = wish.wish
@@ -18,7 +30,7 @@ function routerProvider(app) {
 
   router.post('/wish', ensureLoggedIn, function(req, res, next) {
     var col = app.get('db').collection('test');
-    col.update({'email': req.user.emails[0].value}, {'$set': {'wish': req.body.wish}}, {'upsert': true});
+    col.update({'email': getEmail(req.user)}, {'$set': {'wish': req.body.wish}}, {'upsert': true});
     res.redirect('/user');
   });
 
