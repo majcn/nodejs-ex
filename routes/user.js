51f00543ef26b9fdeb54b6a828d57f16a689d402
Gregor Majcen <majcn.m@gmail.com>
2016-12-15 20:41:18 +0100

    another try

--- routes/user.js
+++ routes/user.js
@@ -18,7 +18,7 @@ function routerProvider(app) {
 
   router.post('/wish', ensureLoggedIn, function(req, res, next) {
     var col = app.get('db').collection('test');
-    col.update({'email': req.user.emails.join()}, {'$set': {'wish': req.body.wish}}, {'upsert': true});
+    col.update({'email': req.user.emails[0].value}, {'$set': {'wish': req.body.wish}}, {'upsert': true});
     res.redirect('/user');
   });
 
